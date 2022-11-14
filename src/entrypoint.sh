c536e6e569faad04f402a85387b33c08978dc40f
chrisjsimpson <chris.j.simpson@live.co.uk>
2022-11-14 23:56:52 +0000

    ability to set EMAIL_API_ENDPOINT

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -ex
 
+# Because incron does not easily support env
+echo -n $EMAIL_API_ENDPOINT > /home/mailbox/EMAIL_API_ENDPOINT
+
 # Start supervisord which start incron & gunicorn
 exec /usr/bin/supervisord
--- src/push-emails.sh
+++ src/push-emails.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-touch /home/mailbox/letters/heartbeat
-set -euxo
+set -uxo
 
 # Adapted from https://github.com/KarmaComputing/send-email-asynchronously/blob/main/push-emails.sh
 
@@ -12,11 +11,12 @@ exec 8>"$LCK";
 flock -x 8;
 
 PATH_TO_EMAILS=$1
+EMAIL_API_ENDPOINT=$(cat /home/mailbox/EMAIL_API_ENDPOINT)
 echo Running push-emails as "$(whoami)"
 # Lossy slow email sender
 for EMAIL in $(find "$PATH_TO_EMAILS" -type f)
 do
   # listen on host example: nc -k -l 1234
-  curl --max-time 2 -v --data-binary @"$EMAIL" host.docker.internal:1234
+  curl --max-time 2 -v --data-binary @"$EMAIL" $EMAIL_API_ENDPOINT
   rm $EMAIL
 done
