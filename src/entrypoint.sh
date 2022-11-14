c28dfc62a70e629a0a555e5608e8f97ce14358c7
chrisjsimpson <chris.j.simpson@live.co.uk>
2022-11-14 21:10:54 +0000

    add incron & supervisord

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
 
-# Start gunicorn, listening on port 500, access log to stdout
-exec gunicorn -w 4 -b '0.0.0.0:5000' --access-logfile=- 'app:app'
+# Start supervisord which start incron & gunicorn
+exec /usr/bin/supervisord
